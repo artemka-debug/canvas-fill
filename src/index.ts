9bc7216369327befbc6c5323e16b2295d1f458ba
artemka-debug <dovgopoly123@gmail.com>
2020-09-19 19:55:10 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import {Request, Response} from 'express-serve-static-core'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
