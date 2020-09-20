4dae51cb6a6f8b3bdaaad6e5bf6cb876bc95bf9b
artemka-debug <dovgopoly123@gmail.com>
2020-09-20 11:20:20 +0300

    test

--- public/src/canvas.ts
+++ public/src/canvas.ts
@@ -26,13 +26,16 @@ export default class Canvas {
 
     public display = (squares: Array<Square>) => {
         const {ctx, canvas} = this;
-
-        if (ctx && canvas) {
-            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        }
-
-        for (const square of squares) {
-            square.display();
+        //
+        // if (ctx && canvas) {
+        //     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        // }
+        //
+        // for (const square of squares) {
+        //     square.display();
+        // }
+        if (ctx) {
+            this.drawHeart(innerWidth / 2, 0, 100, 0, innerWidth, 500, "red");
         }
         requestAnimationFrame(() => this.display(squares));
     };
@@ -48,5 +51,53 @@ export default class Canvas {
     static createInstance = ({id = '', w = 0, h = 0}: CanvasProps) => {
         Canvas.instance = new Canvas({id: id, w: w, h: h});
         return Canvas.instance;
+    };
+
+    public drawHeart = (fromx: number, fromy: number, tox: number, toy: number, lw: number, hlen: number, color: string) => {
+        const {ctx} = this;
+
+        const x = fromx;
+        const y = fromy;
+        const width = lw;
+        const height = hlen;
+
+        if (ctx) {
+            ctx.save();
+            ctx.beginPath();
+            const topCurveHeight = height * 0.3;
+            ctx.moveTo(x, y + topCurveHeight);
+            // top left curve
+            ctx.bezierCurveTo(
+                x, y,
+                x - width / 2, y,
+                x - width / 2, y + topCurveHeight
+            );
+
+            // bottom left curve
+            ctx.bezierCurveTo(
+                x - width / 2, y + (height + topCurveHeight) / 2,
+                x, y + (height + topCurveHeight) / 2,
+                x, y + height
+            );
+
+            // bottom right curve
+            ctx.bezierCurveTo(
+                x, y + (height + topCurveHeight) / 2,
+                x + width / 2, y + (height + topCurveHeight) / 2,
+                x + width / 2, y + topCurveHeight
+            );
+
+            // top right curve
+            ctx.bezierCurveTo(
+                x + width / 2, y,
+                x, y,
+                x, y + topCurveHeight
+            );
+
+            ctx.closePath();
+            ctx.fillStyle = color;
+            ctx.fill();
+            ctx.restore();
+        }
     }
 }
