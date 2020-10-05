d99694f0fea10b288091d700c4e004a7a9515c61
artemka-debug <dovgopoly123@gmail.com>
2020-10-05 18:12:23 +0300

    added zoom buttons and changed the color selector

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express             from 'express';
-import redis               from 'redis';
-import dotenv              from 'dotenv';
-import io                  from 'socket.io';
-import http                from 'http';
+import express from 'express';
+import redis from 'redis';
+import dotenv from 'dotenv';
+import io from 'socket.io';
+import http from 'http';
 import bodyParser from "body-parser";
-import router     from './api';
+import router from './api';
 
 dotenv.config();
 
@@ -20,12 +20,7 @@ export const redisClient = process.env.REDIS_URL ?
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
-socket.on('connection', (socket) => {
-    // socket.emit('connected')
-});
-
-app.use(router);
+app.use('/', router);
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
